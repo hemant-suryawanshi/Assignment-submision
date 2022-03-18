054b3fdb88af8e0eb49e7aa9bf9aeb7013ebe716
Hemant Suryawanshi <97448671+FloatingHeart7@users.noreply.github.com>
2022-03-18 17:47:42 +0530

    Validation

---
+++ Validation/src/models/user.models.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+ 
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    gender: {
+      type: String,
+      enum: ["Male", "Female","Others"],
+      default: "Male",
+    },
+    age: { type: Number, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
