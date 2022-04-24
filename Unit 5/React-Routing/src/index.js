d55d8f0e228d81bfbb198b058a803da6911d0449
Hemant Suryawanshi <97448671+FloatingHeart7@users.noreply.github.com>
2022-04-24 16:02:57 +0530

    Raect-Routing 1&2

---
+++ Unit 5/React-Routing/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/Authcontext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
